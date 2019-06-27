79c33497b16381092b0ab600d7fa950c83fbd926
Emil Thorenfeldt <emt@magenta-aps.dk>
2019-06-27 11:49:19 +0200

    Use ffmpeg instead of vlc, handle linux as well as windows, use pid and OS signals to stop recording

--- RecordFunctions.go
+++ RecordFunctions.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"os"
 	"os/exec"
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
-func captureScreen ()  {
-	var sout = ":sout=#transcode{vcodec=h264,vb072}:standard{access=file,mux=mp4,dst=C:\\BCSVideos\\temp_bcs_recording.mp4}"
-	cmd := exec.Command(`C:\Program Files\VideoLAN\VLC\vlc.exe`, "-I", "dummy","--one-instance", "screen://", ":screen-fps=15", sout)
+var pid int
 
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+func captureScreen (video_software_path string, video_path string)  {
+	cliParams := ""
+	switch runtime.GOOS {
+	case "linux":
+		cliParams += fmt.Sprintf("-y -loglevel error -video_size 1920x1080 -f x11grab -i :0.0 -f pulse -i 0 -f pulse -i default -filter_complex amerge -ac 2 -preset veryfast %stemp_bcs_recording.mp4", video_path)
+		break
+	case "windows":
+		cliParams += fmt.Sprintf("-f dshow -i video=\"screen-capture-recorder\" %stemp_bcs_recording.mp4", video_path)
+		break
+	default:
+		log.Fatal("Unsupported OS: " + runtime.GOOS)
 	}
+	cmd := exec.Command(video_software_path, strings.Split(cliParams, " ")...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+	log.Print("Hello")
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		log.Fatal("Exiting")
+	}
+	pid = cmd.Process.Pid
+	log.Print("Pid is " + strconv.Itoa(pid))
+
+	if err := cmd.Wait(); err != nil {
+		log.Printf("Video recording process has exited: %v", stderr.String())
+	}
+
 }
 
 func stopCapturing () {
-	kill_cmd := exec.Command(`C:\Program Files\VideoLAN\VLC\vlc.exe`, "--one-instance", "vlc://quit")
-
-	if err := kill_cmd.Run(); err != nil {
-		log.Fatal(err)
+	log.Print("Trying to kill " + strconv.Itoa(pid))
+	process, _ := os.FindProcess(pid)
+	err := process.Signal(os.Interrupt)
+	if err != nil {
+		log.Print(err)
 	}
 }

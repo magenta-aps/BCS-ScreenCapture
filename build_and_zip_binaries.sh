c45bdb77912a47c87f6d4d8b54c6530be1a453ac
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-01-23 16:05:15 +0100

    Update default recording params, overwrite instead of append to conf.json.txt

--- build_and_zip_binaries.sh
+++ build_and_zip_binaries.sh
@@ -5,15 +5,15 @@ mkdir -p bin/BCSRecorder_win64/lib
 mkdir -p bin/BCSRecorder_win64/certs
 GOOS=windows GOARCH=amd64 go build -o bin/BCSRecorder_win64/BCSRecorder.exe .
 go build -o bin/BCSRecorder .
-cat << EOF >> bin/conf.json.txt
+cat << EOF > bin/conf.json.txt
 {
   "RECORDING_SOFTWARE_PATH": "lib/ffmpeg/bin/ffmpeg.exe",
-  "RECORDING_SOFTWARE_PARAMS": "-y -f dshow -i audio=virtual-audio-capturer:video=screen-capture-recorder -preset ultrafast %stemp_bcs_recording.%s",
+  "RECORDING_SOFTWARE_PARAMS": "-y -f dshow -i audio=virtual-audio-capturer:video=screen-capture-recorder -filter_complex amix=inputs=1 -vcodec libx264 -pix_fmt yuv420p -preset ultrafast -vsync vfr -acodec libmp3lame %stemp_bcs_recording.%s",
   "CERTIFICATE_PATH": "./certs/",
   "ROOTHOST": "loc.bcomesafe.com",
   "PORT": "3032",
   "VIDEO_SAVE_PATH": "C:/BCSVideos/",
-  "VIDEO_FORMAT": "mpg",
+  "VIDEO_FORMAT": "mp4",
   "TIMEOUT_IN_MINUTES": 60,
   "SAVE_NAME_IN_VIDEO": true,
   "DEBUG": false
